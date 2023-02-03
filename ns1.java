3e7060cc98c068e22639484988264149361c08d7
Anadi Jaiswal <105920531+jaiswalanadi@users.noreply.github.com>
2023-02-03 19:01:35 +0530

    Add files via upload

---
+++ ns1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ns1 {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter the decimal number: ");
+
+        int n = scn.nextInt();
+
+        int binarynum = 0;
+
+        int power = 1;
+
+        while(n != 0){
+
+            int rem = n % 2;
+
+            binarynum += rem * power;
+
+            power *= 10;
+
+            n /= 2;
+
+        }
+
+        System.out.print(binarynum);
+
+    }
+
+}
---
+++ ns2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ns2 {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter the binary number: ");
+
+        int n = scn.nextInt();
+
+        int decimalnum = 0;
+
+        int power = 1;
+
+        while(n != 0){
+
+            int rem = n % 2;
+
+            decimalnum += rem * power;
+
+            power *= 2;
+
+            n /= 10;
+
+        }
+
+        System.out.print(decimalnum);
+
+    }
+
+}
